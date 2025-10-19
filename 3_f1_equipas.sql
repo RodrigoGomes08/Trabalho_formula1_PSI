aa775650adc9617df615cd105ad0f59d79dcd966
Hamburguer234 <joaonicolau2007@gmail.com>
2025-10-19 16:51:53 +0100

    Modelo ER e inserts nas tabelas 2, 3, 4

--- 3_f1_equipas.sql
+++ 3_f1_equipas.sql
@@ -3,9 +3,30 @@ USE f1_system;
 DROP TABLE IF EXISTS equipas;
 
 CREATE TABLE equipas (
-    id INT(11) UNSIGNED AUTO_INCREMENT,
+    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL UNIQUE,
     pais_origem VARCHAR(100) NOT NULL,
-    ano_fundacao YEAR NOT NULL,
+    ano_fundacao INT NOT NULL,
     PRIMARY KEY (id)
 );
+
+INSERT INTO equipas (
+    nome, 
+    pais_origem, 
+    ano_fundacao
+) VALUES
+('Mercedes', 'Alemanha', 1954),
+('Red Bull Racing', 'Áustria', 2005),
+('Ferrari', 'Itália', 1929),
+('McLaren', 'Reino Unido', 1963),
+('Aston Martin', 'Reino Unido', 1913),
+('Alpine', 'França', 1976),
+('Williams', 'Reino Unido', 1977),
+('AlphaTauri', 'Itália', 2006),
+('Haas', 'EUA', 2016),
+('Sauber', 'Suíça', 1993),
+('Renault', 'França', 1977),
+('Toro Rosso', 'Itália', 2006),
+('Lotus', 'Reino Unido', 1958),
+('Benetton', 'Itália', 1986),
+('Brawn GP', 'Reino Unido', 2009);
--- 4_f1_carros.sql
+++ 4_f1_carros.sql
@@ -3,9 +3,31 @@ USE f1_system;
 DROP TABLE IF EXISTS carros;
 
 CREATE TABLE carros (
-    id INT(11) UNSIGNED AUTO_INCREMENT,
+    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     modelo VARCHAR(100) NOT NULL UNIQUE,
     num_carro INT(11) UNSIGNED NOT NULL,
     potencia_motor INT(11) UNSIGNED NOT NULL,
     PRIMARY KEY (id)
 );
+
+
+INSERT INTO carros (
+    modelo, 
+    num_carro, 
+    potencia_motor
+) VALUES
+('W15', 44, 1050),
+('RB20', 1, 1060),
+('SF-24', 16, 1040),
+('MCL38', 4, 1030),
+('AMR24', 14, 1020),
+('A524', 31, 1010),
+('FW46', 23, 1005),
+('AT04', 22, 1015),
+('VF-24', 20, 1000),
+('C44', 77, 1012),
+('W14', 63, 1045),
+('RB19', 11, 1060),
+('SF-23', 55, 1040),
+('MP4/4', 12, 900),
+('BGP001', 22, 950);
