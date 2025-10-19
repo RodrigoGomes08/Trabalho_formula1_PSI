75eec621ee95011662bedb6724c8292d1af07cdd
EstherSouzaP <estherspereira21@gmail.com>
2025-10-19 16:54:51 +0100

    Tabelas pilotos e corridas, com inserts.

--- 5_f1_pilotos.sql
+++ 5_f1_pilotos.sql
@@ -9,3 +9,19 @@ CREATE TABLE pilotos(
     PRIMARY KEY(id)
 );
 
+INSERT INTO pilotos (nome, nacionalidade, data_nascimento, num_camp_ganhos) VALUES
+('Lewis Hamilton', 'Reino Unido', '1985-01-07', 7),
+('Max Verstappen', 'Países Baixos', '1997-09-30', 3),
+('Charles Leclerc', 'Mônaco', '1997-10-16', 0),
+('Lando Norris', 'Reino Unido', '1999-11-13', 0),
+('Fernando Alonso', 'Espanha', '1981-07-29', 2),
+('Esteban Ocon', 'França', '1996-09-17', 0),
+('George Russell', 'Reino Unido', '1998-02-15', 0),
+('Sergio Pérez', 'México', '1990-01-26', 0),
+('Carlos Sainz', 'Espanha', '1994-09-01', 0),
+('Valtteri Bottas', 'Finlândia', '1989-08-28', 0),
+('Oscar Piastri', 'Austrália', '2001-04-06', 0),
+('Lance Stroll', 'Canadá', '1998-10-29', 0),
+('Yuki Tsunoda', 'Japão', '2000-05-11', 0),
+('Nico Hülkenberg', 'Alemanha', '1987-08-19', 0),
+('Ayrton Senna', 'Brasil', '1960-03-21', 3);
--- 6_f1_corridas.sql
+++ 6_f1_corridas.sql
@@ -2,9 +2,26 @@ DROP TABLE IF EXISTS corridas;
 
 CREATE TABLE corridas(
     id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    circuito_id INT(11) UNSIGNED NOT NULL,
+    id_circuito INT(11) UNSIGNED NOT NULL,
     data DATE NOT NULL,
     num_voltas INT(11) NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY(circuito_id) REFERENCES circuitos(id)
+    FOREIGN KEY(id_circuito) REFERENCES circuitos(id)
 );
+
+INSERT INTO Corridas (id_circuito, data, num_voltas) VALUES
+(1, '2024-07-07', 52),
+(2, '2024-09-01', 53),
+(3, '2024-08-25', 44),
+(4, '2024-05-26', 78),
+(5, '2024-09-22', 53),
+(6, '2024-11-03', 71),
+(7, '2024-06-09', 70),
+(8, '2024-06-30', 71),
+(9, '2024-10-20', 56),
+(10, '2024-03-02', 57),
+(11, '2024-12-08', 58),
+(12, '2024-07-21', 70),
+(13, '2024-04-21', 63),
+(14, '2024-08-25', 72),
+(15, '2024-03-09', 50);
